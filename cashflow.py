b4c4d02406b7ac4314ecbf7a766a96229337252a
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-23 10:52:08 +0530

    cashflow dev

--- cashflow.py
+++ cashflow.py
@@ -1,6 +1,17 @@
 import pandas as pd
+import numpy as np
 import streamlit as st
 
 def cash_flow(raw_df):
     df =raw_df.copy()
+    conditions = [
+        df['category'].str.contains('Loan', case=False),
+        df['category'].str.contains('investment', case=False),
+        df['category'].str.contains('salary|dividend', case=False) # '|' acts as OR
+    ]
+
+    choices = ['Loan', 'investment', 'CashIn']
+
+    df['Cash_flow'] = np.select(conditions, choices, default='CashOut')
     st.dataframe(df)
+    
