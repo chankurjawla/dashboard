234e0cf87900492b3a9b96408920392e59f418ac
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-23 14:16:36 +0530

    cash flow

--- cashflow.py
+++ cashflow.py
@@ -16,6 +16,7 @@ def cash_flow(raw_df):
     df['Cash_flow'] = np.select(conditions, choices, default='CashOut')
     df_grouped = df.groupby(['MonthYear','Cash_flow'], as_index=False)['Amount'].sum()
     df_wide = df_grouped.pivot(index='MonthYear', columns='Cash_flow', value='Amount')
+    st.subheader("Cash Flow over last 6 months:")
     st.dataframe(df_wide.tail(6))
     st.divider()
 
