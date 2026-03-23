30befc6268e8c6feb9ca90edb05940cc5b3942f7
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-23 11:02:40 +0530

    cashflow dev

--- cashflow.py
+++ cashflow.py
@@ -1,17 +1,19 @@
 import pandas as pd
 import numpy as np
 import streamlit as st
+import altair as alt
 
 def cash_flow(raw_df):
-    df =raw_df.copy()
+    df = raw_df.copy()
     conditions = [
         df['category'].str.contains('Loan', case=False),
         df['category'].str.contains('investment', case=False),
         df['category'].str.contains('salary|dividend', case=False) # '|' acts as OR
     ]
 
-    choices = ['Loan', 'investment', 'CashIn']
+    choices = ['Loan', 'Investment', 'CashIn']
 
     df['Cash_flow'] = np.select(conditions, choices, default='CashOut')
-    st.dataframe(df)
-    
+    df_grouped = df.groupby(['MonthYear','Cash_flow'], as_index=False)['Amount'].sum()
+    st.dataframe(df_grouped)
+
