6318ab270ae85ff96bc114b3d9b5764d4cdd50d1
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-24 15:06:15 +0530

    dupliacte highlight

--- app.py
+++ app.py
@@ -162,14 +162,24 @@ epfanalysis.render_epf()
 from mf_allotment_pull import push_to_csv
 st.divider()
 st.subheader("Equity:")
-if st.button('Allotment - Ankur'):
-    with st.spinner("Processing..."):
-        push_to_csv("ch.ankurjawla@gmail.com","lupx encd vahn kqym")
-        st.success("MF Allotment report is processed successfully.")
-if st.button('Allotment - Gulu'):
-    with st.spinner("Processing..."):
-        push_to_csv("pooja0626@gmail.com","lupx encd vahn kqym")
-        st.success("MF Allotment report is processed successfully.")
+col1, col2 = st.columns(2)
+with col1:
+    if st.button('Allotment - Ankur'):
+        with st.spinner("Processing..."):
+            push_to_csv("ch.ankurjawla@gmail.com","lupx encd vahn kqym")
+            st.success("MF Allotment report is processed successfully.")
+with col2:
+    if st.button('Allotment - Gulu'):
+        with st.spinner("Processing..."):
+            push_to_csv("pooja0626@gmail.com","lupx encd vahn kqym")
+            st.success("MF Allotment report is processed successfully.")
 equity_file = 'equity_allotment.csv'
 equity_df = pd.read_csv(equity_file)
-st.dataframe(equity_df, use_container_width=True, hide_index=True)
+#st.dataframe(equity_df, use_container_width=True, hide_index=True)
+
+
+def highlight_duplicates(df):
+    is_duplicate = df.duplicated(keep=False)
+    return ['background-color: yellow' if v else '' for v in is_duplicate]
+styled_df = equity_df.style.apply(highlight_duplicates, axis=0)
+st.dataframe(styled_df, use_container_width=True, hide_index=True)
