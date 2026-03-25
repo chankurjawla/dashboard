c8f1af79684ec75d78c1247a0d3719a7018fad07
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-25 13:56:53 +0530

    xdf

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 # Define the pages
 expenses = st.Page("expenses.py", title="Expenses", icon="🎈")
-fin_planning = st.Page("financialplanning.py", title="Financial Planning", icon="❄️")
+fin_planning = st.Page("fin_planning.py", title="Financial Planning", icon="❄️")
 #page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
 
 # Set up navigation
