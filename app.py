164cfee38c4597d5af24d3fac041bd1e37431c15
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-03-25 14:18:03 +0530

    vzv

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import streamlit as st
 # Define the pages
 expenses = st.Page("expenses.py", title="Expenses", icon="🎈")
 fin_planning = st.Page("fin_planning.py", title="Financial Planning", icon="❄️")
-#page_3 = st.Page("page_3.py", title="Page 3", icon="🎉")
+archived_app = st.Page("archived_app.py", title="Financial Analytics", icon="🎉")
 
 # Set up navigation
-pg = st.navigation([expenses, fin_planning])
+pg = st.navigation([expenses, fin_planning, archived_app])
 
 # Run the selected page
 pg.run()
--- archived_app.py
+++ archived_app.py
@@ -104,3 +104,56 @@ with st.expander(f"Detailed Spending: {selected_month} | {selected_cats}"):
 
 st.divider()
 
+# --- 6. Equity Trend (Ultra-Fast Strategy) ---
+st.subheader("Equity Analysis")
+
+# Button to trigger the heavy work
+if st.button('Refresh Equity Data'):
+    with st.spinner("Calculating Equity Values..."):
+        totalequityvalue().to_csv('total-equity-value.csv', index=False)
+        st.rerun()
+
+# Instant Load from CSV
+EQUITY_FILE = 'total-equity-value.csv'
+if os.path.exists(EQUITY_FILE):
+    equity_df = pd.read_csv(EQUITY_FILE)
+    tab1, tab2= st.tabs(["Equity Trend", "Table"])
+    with tab1:
+        chart = alt.Chart(equity_df).mark_line(point=True).encode(
+            x='Date:T',
+            y='TotalValue:Q',
+            tooltip=['Date', 'TotalValue']
+        ).properties(height=400)
+        st.altair_chart(chart, use_container_width=True)
+    with tab2:
+        st.dataframe(equity_df, use_container_width=True, hide_index=True)
+else:
+    st.info("No Equity data found. Click 'Refresh Equity Data' to generate.")
+
+st.divider()
+
+# --- 7. Sectoral Indices (Ultra-Fast Strategy) ---
+st.subheader('Sectoral Indices')
+
+# Button to trigger the 40-second PDF scan
+if st.button('Refresh Sectoral Indices'):
+    with st.spinner("Processing PDFs (takes ~40s)..."):
+        # Calling the function from sectoral_indices.py
+        result_df = si.sectoral_indices()
+        if not result_df.empty:
+            result_df.to_csv('sectoral_indices.csv', index=False)
+            st.success("Successfully updated indices!")
+            st.rerun()
+        else:
+            st.error("No data extracted from PDFs.")
+
+# Instant Load from CSV
+SECTOR_FILE = 'sectoral_indices.csv'
+if os.path.exists(SECTOR_FILE):
+    sectoral_df = pd.read_csv(SECTOR_FILE)
+    st.dataframe(sectoral_df, use_container_width=True, hide_index=True)
+else:
+    st.info("No Sectoral data found. Click 'Refresh Sectoral Indices' to start PDF scan.")
+
+# --- 8. EPF Analysis 
+epfanalysis.render_epf()
