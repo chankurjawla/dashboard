c6e4c0a86210ee1941fcbc19940ceb48c66cb4e2
chankurjawla <144441133+chankurjawla@users.noreply.github.com>
2026-08-01 12:28:28 +0530

    vhmvjh

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ import streamlit as st
 
 # Define the pages
 expenses = st.Page("expenses.py", title="Financial Analytics", icon="💰")
-expenses = st.Page("expenses_home.py", title="Home Purchase", icon="🏠")
+expenses_home = st.Page("expenses_home.py", title="Home Purchase", icon="🏠")
 fin_planning = st.Page("fin_planning.py", title="Investment Analytics", icon="🦈")
 archived_app = st.Page("archived_app.py", title="Financial Analytics - Archived", icon="💾")
 
 # Set up navigation
-pg = st.navigation([expenses, fin_planning, archived_app])
+pg = st.navigation([expenses, expenses_home, fin_planning, archived_app])
 
 # Run the selected page
 pg.run()
